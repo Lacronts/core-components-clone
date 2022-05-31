843d90af472fb8ba5c8a6e61733de403d5f11149
Vladimir Gevak <VGevak@alfabank.ru>
2022-05-31 12:25:14 +0700

    fix(*): release

--- release.config.js
+++ release.config.js
@@ -15,5 +15,5 @@ module.exports = {
         '@semantic-release/github',
     ],
     branches: ['master'],
-    repositoryUrl: 'https://github.com/core-ds/core-components',
+    repositoryUrl: 'https://github.com/Lacronts/core-components-clone',
 };
