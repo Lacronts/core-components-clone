0ff20f4a3462ab1b5de9a80de91b9d89cd562ac4
Vladimir Gevak <VGevak@alfabank.ru>
2022-07-14 14:39:44 +0700

    fix(*): publish

--- bin/publish-beta-ci.sh
+++ bin/publish-beta-ci.sh
@@ -5,8 +5,6 @@ set -e
 
 cd dist
 
-npm config set _auth $NPM_TOKEN && npm config set registry "https://registry.npmjs.org" && npm config set always-auth=true && npm config set email=ikuz58@gmail.com
-
 npm version "$VERSION" --no-git-tag-version
 
 npm publish --tag beta
