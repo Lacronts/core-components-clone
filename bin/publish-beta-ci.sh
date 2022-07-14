454e446d35549be9696987ce2d61025f8ffc959f
Vladimir <ikus58@gmail.com>
2022-07-14 13:38:47 +0700

    fix(*): publish

--- bin/publish-beta-ci.sh
+++ bin/publish-beta-ci.sh
@@ -5,6 +5,8 @@ set -e
 
 cd dist
 
+npm config set _auth $NPM_TOKEN && npm config set registry "https://registry.npmjs.org" && npm config set always-auth=true && npm config set email=ikuz58@gmail.com
+
 npm version "$VERSION" --no-git-tag-version
 
 npm publish --tag beta
